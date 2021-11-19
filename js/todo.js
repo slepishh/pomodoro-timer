0518a54f83440ec65e06b8a275620a5f333f73b1
slepishh <83390207+slepishh@users.noreply.github.com>
2021-11-19 09:00:35 +0300

    Add files via upload

---
+++ js/todo.js
@@ -0,0 +1,33 @@
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+
+
+
+
+
+
+
+input.onkeypress = function(event) {
+
+    if (event.keyCode === 13) {
+
+        let li = document.createElement("li");
+
+
+
+        li.innerHTML = input.value;
+
+        document.querySelector("ul").appendChild(li);
+
+        input.value = "";
+
+    }
+
+};
+document.querySelector("ul").onclick = function(event) {
+    let target = event.target;
+    if (target.tagName == 'LI') {
+        target.classList.toggle('finish');
+    }
+}
