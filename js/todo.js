1a8f95eafcd63ad0d5d2ea81c84fc3893ba8b242
slepishh <83390207+slepishh@users.noreply.github.com>
2021-11-26 08:23:23 +0300

    мелкие исправления

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,5 @@
 let ul = document.querySelector("ul");
-let input = document.querySelector("input");
-
-
-
-
-
-
+let input = document.querySelector(".to-do__write");
 
 
 input.onkeypress = function(event) {
@@ -13,8 +7,6 @@ input.onkeypress = function(event) {
     if (event.keyCode === 13) {
 
         let li = document.createElement("li");
-
-
 
         li.innerHTML = input.value;
 
@@ -29,6 +21,5 @@ document.querySelector("ul").onclick = function(event) {
     let target = event.target;
     if (target.tagName == 'LI') {
         target.classList.toggle('finish');
-        console.log(event)
     }
 }
